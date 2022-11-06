e703670c68c39719814e4a2497c1861bf4a8e20a
emm-ac <emmanuel.ac6@gmail.com>
2022-11-05 23:40:24 -0300

    3ra. carga

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/proyecto1_sup07")
-async def index():
-    return {"Hola Mundo"}
+@app.get("/my-firts-api")
+def get_tablas():
+
+    import pandas as pd
+    url ='https://github.com/emm-ac/proyecto1_sup07/blob/dad400563892759d73987edc3d1c0a21e925721b/Netflix_ok.csv'
+    tablas = pd.read_csv(url)
+
+    return tablas
