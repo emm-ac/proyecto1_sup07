9388116fdaa527c4b4ff60908e0e93ffde45b7e5
emm-ac <emmanuel.ac6@gmail.com>
2022-11-06 14:30:58 -0300

    actualizacion de main.py

--- app/main.py
+++ app/main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Bienvenido expectador serial, de que año quieres ver la info?: 2019 / 2020 / 2021"}
