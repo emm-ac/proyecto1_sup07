3e7fa62fd9131f00afd302d6d4e35044631de72a
emm-ac <emmanuel.ac6@gmail.com>
2022-11-06 14:27:52 -0300

    cambios en main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/hello")
-async def hello():
-    return {'message':'Mongo:bongo'}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
